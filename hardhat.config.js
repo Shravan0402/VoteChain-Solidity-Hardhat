d56bb3ebc24ba7b57e14aca3a00507ef3f2bacfd
Arman Aurobindo <arman8895884469@.com>
2022-09-11 01:53:49 +0530

    Adding deploy scripts and some minimal changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('hardhat-deploy');
+require('dotenv').config()
+RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+PRIVATE_RINKEBY_KEY = process.env.PRIVATE_RINKEBY_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    hardhat:{
+      chainId: 31337,
+    },
+    rinkeby:{
+      chainId:4,
+      url: RINKEBY_RPC_URL,
+      accounts:[PRIVATE_RINKEBY_KEY]
+    }
+  },
 };
---
+++ scripts/deployERC721.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    const tokenERC721Contract = await hre.ethers.getContractFactory("TokenERC721")
+    const tokenERC721 = await tokenERC721Contract.deploy()
+    await tokenERC721.deployed()
+    console.log(tokenERC721.address)
+}
+  
+  
+main().catch((error) => {
+console.error(error);
+process.exitCode = 1;
+});
+  
